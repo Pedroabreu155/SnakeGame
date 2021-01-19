15dffc685ad751df64e0ff11a163522f2cbe4d2e
Pedroabreu155 <pedroliveirabreu@gmail.com>
2021-01-19 17:35:44 -0300

    comida surgindo aleatoriamente na tela

--- script.js
+++ script.js
@@ -9,9 +9,13 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box,
+}
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "DarkSeaGreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
@@ -20,6 +24,11 @@ function criarCobra() {
         context.fillStyle = "olive",
             context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood() {
+    context.fillStyle = "Crimson";
+    context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener('keydown', update);
@@ -41,6 +50,7 @@ function iniciarGame() {
 
     criarBG();
     criarCobra();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
