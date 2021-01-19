b388dae2fe0e7fb8bdf403b3019df425793cc89b
Pedroabreu155 <pedroliveirabreu@gmail.com>
2021-01-19 18:25:57 -0300

    JOGO FINALIZADO, começando a estilizar

--- script.js
+++ script.js
@@ -48,6 +48,13 @@ function iniciarGame() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over  ;-; \nRecarregue a Página para jogar Novamente\nMade by: Pedro Abr3us o brabo!!!');
+        }
+    }
+
     criarBG();
     criarCobra();
     drawFood();
